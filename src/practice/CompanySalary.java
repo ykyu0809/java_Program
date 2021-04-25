ed2ac8a2e6240b24035c6dd7fee64973c7a86656
aml <2523541169@qq.com>
2021-04-25 19:46:50 +0800

    END

--- src/practice/CompanySalary.java
+++ src/practice/CompanySalary.java
@@ -4,25 +4,34 @@ abstract class Employee {
     public abstract double earnings();
 }
 class YearWorker extends Employee {
-    public  double earnings() {
-        return  12000;
+    @Override
+    public double earnings() {
+        return 12000;
     }
 }
 class MonthWorker extends Employee {
+    @Override
     public double earnings() {
-        return 12 * 2300;
+        return 12*2300;
     }
 }
 class WeekWorker extends  Employee {
+    @Override
     public double earnings() {
         return 52*780;
+    }
+}
+class DayWorker extends Employee{
+    @Override
+    public double earnings() {
+        return 100;
     }
 }
 class Company {
     Employee[] employee;
     double salaries=0;
-    Company(Employee[] employees) {
-        this.employee=employees;
+    Company(Employee[] employee) {
+        this.employee=employee;
     }
     public double salariesPay() {
         salaries = 0;
@@ -36,9 +45,10 @@ public class CompanySalary {
     public static void main(String args[]) {
         Employee [] employee=new Employee[29];
         for (int i=0;i<employee.length;i++) {
-            if (i%3==0) employee[i]=new WeekWorker();
-            else if (i%3==1) employee[i]=new MonthWorker();
-            else if (i%3==2) employee[i]=new YearWorker();
+            if (i%4==0) employee[i]=new WeekWorker();
+            else if (i%4==1) employee[i]=new MonthWorker();
+            else if (i%4==2) employee[i]=new YearWorker();
+            else if (i%4==3) employee[i]=new DayWorker();
         }
         Company company=new Company(employee);
         System.out.println("公司薪水总额"+company.salariesPay()+"元");
