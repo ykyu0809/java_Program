d4efead4bf915bfc01284055dcaa9ddb3f466f94
aml <2523541169@qq.com>
2021-05-22 00:02:23 +0800

    Merge remote-tracking branch 'new/master'
    # Conflicts:
    #	src/Book/JFrame/MyFrame.java

---
+++ src/Book/JFrame/ShowCalander.java
@@ -0,0 +1,53 @@
+package Book.JFrame;
+
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.Arrays;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.GregorianCalendar;
+
+public class ShowCalander extends JFrame {
+    ShowCalander(String title){
+        setLayout(null);
+        setTitle(title);
+
+        CalendarBean cb = new CalendarBean();
+        cb.setMonth(5);
+        cb.setYear(2021);
+        String [] a = cb.getCalendar();
+        char [] str = "日一二三四五六".toCharArray();
+
+        JButton button4 = new JButton("显示本月日历");
+        Date nowTime = new Date();
+        add(button4);
+        button4.setBounds(20,10,120,30);
+        JLabel label4 = new JLabel();
+        add(label4);
+        label4.setBounds(160,10,220,30);
+        JTextArea text2 = new JTextArea();
+        add(text2);
+        text2.setBounds(0,50,400,300);
+        text2.setEditable(false);
+        text2.setOpaque(false);
+        text2.setLineWrap(true);
+        ActionListener listener4 = new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                StringBuffer s = new StringBuffer();
+                label4.setText(nowTime.toString());
+                for (char c : str){
+                    text2.append("    "+c+"           ");
+                }
+                text2.append("\r\n"+"   ");
+                for (int i=0;i<a.length;i++){
+                    if(i%7==0) text2.append("\r\n\r\n");
+                    if(i<10)  text2.append("       "+a[i]+"          ");
+                    else text2.append("      "+a[i]+"        ");
+                }
+            }
+        };
+        button4.addActionListener(listener4);
+    }
+}
